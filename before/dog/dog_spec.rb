b0a2d29580e6aa074da0c9834662195875f6f447
Clayton Lengel-Zigich <Clayton Lengel-Zigich clayton@claytonlz.com>
2010-06-21 21:03:50 -0700

    Explicitly require the correct Dog class

--- before/dog/dog_spec.rb
+++ before/dog/dog_spec.rb
@@ -1,5 +1,5 @@
-require File.join(File.dirname(File.expand_path(__FILE__)),"..", "..", "spec_helper.rb")
-require 'dog'
+require File.join(File.dirname(File.expand_path(__FILE__)),"..", "..", "spec_helper")
+require File.join(File.dirname(File.expand_path(__FILE__)),"dog")
 
 describe "Dog" do
   describe "speak" do
